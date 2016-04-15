5dc9e6b595b23b584fae0af582e712145db9404b
Anoop John <anoop21linkdin@gmail.com>
2016-04-15 11:36:34 +0530

    url fix

--- apps/blogs/urls.py
+++ apps/blogs/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.BlogListView.as_view(), name="blog_list"),
-    url(r'^(?P<slug>[-\w]+)/$', views.BlogDetailView.as_view(), name='blog_detail'),
+    url(r'^(?P<slug>[-\w]+)', views.BlogDetailView.as_view(), name='blog_detail'),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
